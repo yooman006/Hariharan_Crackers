e05a70412b0217d3a1ec6306fcc9aa6f5f3bcbe6
yooman006 <barathvikraman6523@gmail.com>
2025-09-13 12:17:47 +0530

    Commit
    Signed-off-by: yooman006 <barathvikraman6523@gmail.com>

--- Haricrackers/src/services/orderServices.jsx
+++ Haricrackers/src/services/orderServices.jsx
@@ -1,5 +1,5 @@
 export const submitOrder = async (orderData) => {
-  const response = await fetch('http://localhost:5000/api/orders', {
+  const response = await fetch('https://hariharan-crackers-1.onrender.com/api/orders', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
